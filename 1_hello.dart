91ad38dbd60a8804302d62d895ceb25ad567408a
theeasylearn <theeasylearn@gmail.com>
2026-08-01 18:33:30 +0530

    first commit

---
+++ 1_hello.dart
@@ -0,0 +1,12 @@
+/* 
+    first program in dart language 
+    Date : 01-08-2026 
+    Author : The easylearn academy
+*/
+void main()
+{
+    //use // comment for one line comment 
+    print("Hello world we have started learning dart."); // this is also comment
+    print("Learning dart is like learning c language");
+    print("good bye");
+}
---
+++ 2_variables.dart
@@ -0,0 +1,23 @@
+void main()
+{
+    int age = 21;
+    double weight = 75.12345;
+    bool gender = true;
+    String name = "the easylearn academy";
+
+    //print variables value 
+    print(age);
+    print(weight);
+    print(gender);
+    print(name);
+
+    print("Name = $name");
+    print("Weight = $weight");
+    print("gender = $gender");
+    print("age = $age");
+
+    print("age = " + age.toString());
+    print("weight = " + weight.toString());
+    print("gender = " + gender.toString());
+    print("name = " + name);
+}
